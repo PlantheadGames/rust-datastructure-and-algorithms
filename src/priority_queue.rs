ef7e2e223ac7f81543a876b263742259f46a2c83
nown992 <nick.owen992@gmail.com>
2026-03-22 22:25:54 +0000

    added based structure for Priority Queue

---
+++ src/priority_queue.rs
@@ -0,0 +1,34 @@
+
+
+struct PriorityQueue<T>{
+
+}
+
+impl PriorityQueue {
+    fn new() -> Self{
+        todo!();
+    }
+     fn peek(){
+        todo!();
+    }   
+    fn pop(){
+        todo!();
+    }
+
+    fn push(element: <T>, priority: <U>){
+        todo!();
+    }
+    fn remove(element: <T>){
+        todo!();
+    }
+    fn update(element:<T>, priority: <U>){
+        todo!();
+    }
+
+
+
+
+
+
+
+}
